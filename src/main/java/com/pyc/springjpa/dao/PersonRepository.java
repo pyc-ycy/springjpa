8965477d36dd671e74e15035b6ba37102c7c9e41
ycy-ycy <2923616405@qq.com>
2020-02-09 12:06:24 +0800

    update

--- src/main/java/com/pyc/springjpa/dao/PersonRepository.java
+++ src/main/java/com/pyc/springjpa/dao/PersonRepository.java
@@ -15,12 +15,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person,Long> {
+    // 根据地址查询，返回值为列表
     List<Person> findByAddress(String address);
-
+    // 根据姓名和地址查询，返回值为单个对象
     Person findByNameAndAddress(String name, String address);
-
+    // 使用 @Query 查询，参数按照名称绑定
     @Query("select p from Person p where p.name= :name and p.address= :address")
     Person withNameAndAddressQuery(@Param("name")String name,@Param("address")String address);
+    //使用 @NamedQuery 查询，在实体类中已注解
     Person withNameAndAddressNamedQuery(String name, String address);
 
 }
